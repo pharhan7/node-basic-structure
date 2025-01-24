5c83e754462cf39c387137d82240c2c068e1539a
pharhan7 <developerfarhan7@gmail.com>
2025-01-24 10:48:30 +0530

    with cluster and workers

--- index.js
+++ index.js
@@ -2,21 +2,41 @@ require('dotenv').config();
 const app = require("./src/app.js");
 const {db} = require("./config/index.js");
 const mongoose = require('mongoose');
+const cluster = require('cluster');
+const os = require('os');
 
-(
-    async () => {
-        try {
-            await db();
-            console.log("Database connected successfully");
-            
-            app.listen(process.env.PORT, () => {
-                console.log(`Server is running on port ${process.env.PORT}`);
-            });
-        } catch (error) {
-            console.error("Database connection failed: ", error);
-        }
+const numCPUs = os.cpus().length;
+
+if (cluster.isMaster) {
+    console.log(`Master ${process.pid} is running`);
+
+    // Fork workers.
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
     }
-)()
+
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`Worker ${worker.process.pid} died`);
+    });
+} else {
+    console.log(`Worker ${process.pid} started`);
+    (
+        async () => {
+            try {
+                await db();
+                console.log("Database connected successfully");
+                
+                app.listen(process.env.PORT, () => {
+                    console.log(`Server is running on port ${process.env.PORT}`);
+                });
+            } catch (error) {
+                console.error("Database connection failed: ", error);
+            }
+        }
+    )();
+}
+
+
 function shutdown() {
     mongoose.connection.close(() => {
         console.log('Mongoose connection disconnected due to app termination');
